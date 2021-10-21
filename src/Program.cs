7589991009444fa8665012e112350e8c1b41d27e
Angelo <angelo.lamonaca@gmail.com>
2021-10-21 23:09:10 +0200

    🎉 First ConsoleChessGame Project Commit

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleChessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
